bcbfdbfdfc18b78b8bb3240ac723fb190e0a5bcb
Andrew Grogan-Kaylor <agrogan@umich.edu>
2023-02-15 12:16:24 -0500

    tried PPTX; doesn't work too well; re-rendered as HTML

--- paper-template/myrender.R
+++ paper-template/myrender.R
@@ -4,5 +4,6 @@ quarto::quarto_render(
   "./paper-template/cross-sectional-multilevel.qmd", 
   output_format = c("pdf", 
                     "html", 
-                    "docx"))
+                    "docx",
+                    "pptx"))
 
