76ab44fa0f342d25f37bcbba2b1a416b62966156
Grogan-Kaylor <agrogan@UMICH.EDU>
2023-10-16 14:29:41 -0400

    clarifications & cleanup

--- paper-template/myrender.R
+++
@@ -1,9 +0,0 @@
-# render document to multiple formats
-
-quarto::quarto_render(
-  "./paper-template/cross-sectional-multilevel.qmd", 
-  output_format = c("pdf", 
-                    "html", 
-                    "docx",
-                    "pptx"))
-
